be478b315a3be86ba9fe9954ed9d0e8e74ad732b
hayashi28 <ellegarden1004@gmail.com>
2016-10-23 12:36:00 +0900

    make groups table

--- db/migrate/20161023032230_create_messages.rb
+++ db/migrate/20161023032230_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text       :body
-      t.string     :image
+      t.text        :body
+      t.string      :image
       t.references  :user
       t.references  :group
       t.timestamps
